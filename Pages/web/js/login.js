28d9f5f5567b5548be8e876a88f798542c5c5ccb
zaizai <794981961@qq.com>
2020-05-07 00:59:03 +0800

    add Pages

---
+++ Pages/web/js/login.js
@@ -0,0 +1,33 @@
+$(document).ready(
+    function () {
+        // var condition = $("condition").text;
+        $("button").click(function () {
+            ajaxObj = $.ajax({
+                type:"get",
+                contentType: "application/json;charset=UTF-8",
+                url:"http://193.112.176.224:8080/Web_war/service",
+                async:true,
+                data:{
+                    "condition":"111",
+                    "insert_type":"1",
+                    "userName":$("#username").val(),
+                    "password":$("#password").val(),
+                },
+                success:function () {
+                    var json = eval("("+ajaxObj.responseText+")");
+                    if($("#password").val()!=json.password){
+                        alert("login error");
+                    }else{
+                        alert("login ok");
+                    }
+                },
+                error:function () {
+                    // alert("error");
+                    $("#result").html(ajaxObj.responseText);
+
+                    $("input[type='text']").val("");
+                }
+            });
+        });
+    }
+);
